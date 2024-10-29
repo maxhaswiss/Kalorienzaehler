9b493d00c0cadae51f3a9d2053290ce394001b61
lunar-noon <derherogamer@eclipso.ch>
2024-10-29 13:36:46 +0100

    folder change

--- kalorienzaehler_java/backend/src/main/java/kalorienzaehler/backend/repository/MealRepository.java
+++
@@ -1,9 +0,0 @@
-package kalorienzaehler.backend.repository;
-
-import kalorienzaehler.backend.entity.Meal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MealRepository extends JpaRepository<Meal, Long> {
-}
