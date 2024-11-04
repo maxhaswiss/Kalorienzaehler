30a55bcc0ab834a22db761779d792113a10f4ec8
zer0 <max.hasselwander@wiss-edu.ch>
2024-11-04 13:52:46 +0100

    tried to connect frontend with backend

--- node_modules/kalorienzaehler/node_modules/kalorienzaehler/kalorienzaehler_java/backend/src/main/java/kalorienzaehler/backend/repository/CaloricIntakeRepository.java
+++
@@ -1,9 +0,0 @@
-package main.java.kalorienzaehler.backend.repository;
-
-import main.java.kalorienzaehler.backend.entity.CaloricIntake;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CaloricIntakeRepository extends JpaRepository<CaloricIntake, Long> {
-}
